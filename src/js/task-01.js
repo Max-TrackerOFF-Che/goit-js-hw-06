60afa11b853ba47c437d36bfabbf1e48f57f3cf6
Maksym Chepurnyak <makassi12@gmail.com>
2023-09-25 14:11:55 +0300

    Update task 1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,10 +1,11 @@
 const totalCategories = document.querySelectorAll(".item");
 console.log(`Number of categories: ${totalCategories.length}`);
 
-const categoriesArray = [...totalCategories]
-  .map(
-    categories => `Category: ${categories.children[0].textContent}
-Elements: ${categories.children[1].children.length}`
-  )
-  .join("\n"+"\n");
-console.log(categoriesArray);
+const categoriesArray = [];
+totalCategories.forEach((category) => {
+  const categoryName = category.children[0].textContent;
+  const elementCount = category.children[1].children.length;
+  categoriesArray.push(`Category: ${categoryName}\nElements: ${elementCount}`);
+});
+
+console.log(categoriesArray.join("\n\n"));
